814eb24eeefbb47eeaaa1021adaf04b07a0f93c9
Kevin Quinn <kevinquinnw@gmail.com>
2020-12-08 19:34:31 -0500

    Add files via upload

---
+++ negative-binomial-regression.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from patsy import dmatrices
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+from typing import Dict
+
+
+def negative_binomial_graph(data: Dict, state: str) -> None:
+    """A function to display a negative binomial regression model graphically"""
+
+    pd.options.mode.chained_assignment = None
+
+    # First, creating a pandas dataframe for our data
+    df = pd.DataFrame.from_dict(data[state], orient='index', columns=['Average_Temp', 'Precipitation', 'Fire_Counts'])
+
+    # Creating a regression variable for the Year
+    df['Year'] = df.index
+
+    # Creates a random sample from a uniform distribution
+    rand_samples = np.random.rand(len(df)) < 0.8
+
+    # Training dataset
+    df_train = df[rand_samples]
+
+    # Testing dataset
+    df_test = df[~rand_samples]
+
+    # Regression expression in Patsy Notation
+    equation = """Fire_Counts ~ Average_Temp + Precipitation + Year"""
+
+    # Creating matrices for the training datasets w/ Patsy
+    y_train, x_train = dmatrices(equation, df_train, return_type='dataframe')
+
+    # Creating matrices for the testing datasets w/ Patsy
+    y_test, x_test = dmatrices(equation, df_test, return_type='dataframe')
+
+    # Using statsmodels GLM class to train the regression
+    poisson_results = sm.GLM(y_train, x_train, family=sm.families.Poisson()).fit()
+
+    # Adding the Lambda vector as a new column in our df
+    df_train['Count_Mean'] = poisson_results.mu
+
+    # Add a column to store values of dependent variable
+    df_train['OLS_Variable'] = df_train.apply(
+        lambda x: ((x['Fire_Counts'] - x['Count_Mean']) ** 2 - x['Count_Mean']) / x['Count_Mean'], axis=1)
+
+    # Model specification for OSL Regression
+    ols_equation = """OLS_Variable ~ Count_Mean - 1"""
+
+    ols_reg_results = smf.ols(ols_equation, df_train).fit()
+
+    nb2_training_results = sm.GLM(y_train, x_train,
+                                  family=sm.families.NegativeBinomial(alpha=ols_reg_results.params[0])).fit()
+
+    nb2_predictions = nb2_training_results.get_prediction(x_test)
+
+    predictions_summary_frame = nb2_predictions.summary_frame()
+
+    predicted_counts = predictions_summary_frame['mean']
+    actual_counts = y_test['Fire_Counts']
+    fig = plt.figure()
+    fig.suptitle('Predicted versus actual wildfire counts in' + ' ' + state)
+    predicted, = plt.plot(x_test.index, predicted_counts, 'go-', label='Predicted counts')
+    actual, = plt.plot(x_test.index, actual_counts, 'ro-', label='Actual counts')
+    plt.legend(handles=[predicted, actual])
+    plt.show()
---
+++ poisson_regression.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from patsy import dmatrices
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from typing import Dict
+
+
+def poisson_graph(data: Dict, state: str) -> None:
+    """A function to display a poisson regression model graphically"""
+
+    # First, creating a pandas dataframe for our data
+    df = pd.DataFrame.from_dict(data[state], orient='index', columns=['Average_Temp', 'Precipitation', 'Fire_Counts'])
+
+    # Creating a regression variable for the Year
+    df['Year'] = df.index
+
+    # Creates a random sample from a uniform distribution
+    rand_samples = np.random.rand(len(df)) < 0.8
+
+    # Training dataset
+    df_train = df[rand_samples]
+
+    # Testing dataset
+    df_test = df[~rand_samples]
+
+    # Regression expression in Patsy Notation
+    equation = """Fire_Counts ~ Average_Temp + Precipitation + Year"""
+
+    # Creating matrices for the training datasets w/ Patsy
+    y_train, x_train = dmatrices(equation, df_train, return_type='dataframe')
+
+    # Creating matrices for the testing datasets w/ Patsy
+    y_test, x_test = dmatrices(equation, df_test, return_type='dataframe')
+
+    # Using statsmodels GLM class to train the regression
+    poisson_results = sm.GLM(y_train, x_train, family=sm.families.Poisson()).fit()
+
+    poisson_predictions = poisson_results.get_prediction(x_test)
+
+    # Returning a pandas DF w .summary_frame()
+    predictions_summary_frame = poisson_predictions.summary_frame()
+
+    # Extracting the mean values for predicted counts
+    predicted_counts = predictions_summary_frame['mean']
+
+    actual_counts = y_test['Fire_Counts']
+
+    # Creating the plot
+
+    fig = plt.figure()
+
+    fig.suptitle('Predicted versus actual wildfire counts in' + ' ' + state)
+
+    predicted, = plt.plot(x_test.index, predicted_counts, 'go-', label='Predicted counts')
+    actual, = plt.plot(x_test.index, actual_counts, 'ro-', label='Actual counts')
+    plt.legend(handles=[predicted, actual])
+    plt.show()
